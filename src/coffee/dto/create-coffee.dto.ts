570e45cdb23086b1c7f7a697d5b5ff666ea3e352
IbnuHs <ibnuhs30@gmail.com>
2023-10-02 10:39:11 +0800

    add handler in service and controller

--- src/coffee/dto/create-coffee.dto.ts
+++ src/coffee/dto/create-coffee.dto.ts
@@ -1,1 +1,24 @@
-export class CreateCoffeeDto {}
+import { IsNotEmpty } from 'class-validator';
+
+export class CreateCoffeeDto {
+  @IsNotEmpty()
+  Menu: string;
+
+  @IsNotEmpty()
+  Rating: string;
+
+  @IsNotEmpty()
+  Category: string;
+
+  @IsNotEmpty()
+  Reviewer: string;
+
+  @IsNotEmpty()
+  Description: string;
+
+  @IsNotEmpty()
+  Price: string;
+
+  @IsNotEmpty()
+  imgUrl: string;
+}
